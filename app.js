b45016aac2531aa9e5197620f4bf4437bb78ccc8
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2017-03-15 21:34:27 +0100

    Test moteur de template ejs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 let app = require('express')()
 
-app.get('/',(req,res)=>{
-    
-    res.send('salut')
-    
+// Moteur de Templates / modèles EJS 
+app.set('view engine', 'ejs')
+
+// Test de l'app
+app.get('/', (req, res) => {
+
+    res.render('index', {
+        title: 'Vote - CatMash'
+    });
+
 })
 
 app.listen(3001)
